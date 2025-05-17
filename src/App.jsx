ded653d9c049470668a696fb1c9bc600b46768c0
suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-05-18 05:48:53 +0900

    header-how

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
+
+export function ArrowIcon() {
+  const [color, setColor] = useState("#333333"); // 더 진한 색으로 수정
+  const [text, setText] = useState("식사 장소");
+
+  return (
+    <svg
+      className="w-full h-auto" // 크기 조정 추가
+      viewBox="0 0 272 131"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M226.667 0L272 65.5L226.667 131H0L0 0L226.667 0Z" fill={color} />
+      <rect
+        x="60"
+        y="53.5"
+        width="24"
+        height="24"
+        stroke="#E20717"
+        strokeWidth="3"
+      />
+      <text x="100" y="76" fill="black" fontSize="32" textAnchor="start">
+        {text}
+      </text>
+    </svg>
+  );
+}
 
 function App() {
   const [page, wherePage] = useState(1);
@@ -30,31 +57,26 @@ function App() {
         </div>
 
         {/*메인 콘텐츠 */}
-        <div className="w-4/5 h-full flex flex-col items-center px-4 py-8 bg-[#F5F5F5]">
-          <p className="text-1xl mb-4 text-left w-full font-bold">
-            결제 방법을 선택해 주세요.
-          </p>
-
-          {/* 결제 버튼들 */}
-          <div className="grid grid-cols-1 gap-4 w-full items-center justify-items-center">
-            <button className="w-11/12 h-16 text-lg font-bold rounded-xl bg-white">
-              카드 결제
-            </button>
-
-            <button
-              className="border-2 border-black w-11/12 h-16 text-lg font-semibold rounded-xl"
-              style={{ backgroundColor: "#FFC915", color: "#000000" }}
-            >
-              간편 결제
-            </button>
-
-            <button
-              className="border-2 border-black w-11/12 h-16 text-lg font-semibold rounded-xl"
-              style={{ backgroundColor: "#FFC915", color: "#000000" }}
-            >
-              모바일 상품권
-            </button>
-          </div>
+        <div className="grid grid-cols-4 w-full items-center justify-items-center border border-red-500 max-w-[272px]">
+          <ArrowIcon />
+          <ArrowIcon />
+          <ArrowIcon />
+          <ArrowIcon />
+          <button className="w-11/12 h-16 text-lg font-bold rounded-xl bg-white">
+            카드 결제
+          </button>
+          <button
+            className="border-2 border-black w-11/12 h-16 text-lg font-semibold rounded-xl"
+            style={{ backgroundColor: "#FFC915", color: "#000000" }}
+          >
+            간편 결제
+          </button>
+          <button
+            className="border-2 border-black w-11/12 h-16 text-lg font-semibold rounded-xl"
+            style={{ backgroundColor: "#FFC915", color: "#000000" }}
+          >
+            모바일 상품권
+          </button>
         </div>
       </div>
     </div>
